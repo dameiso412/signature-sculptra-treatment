831c31dc7fb21ef3932cc38f303696b6d6bb42bd
dameiso412 <damaso@sacamedi.com>
2025-03-21 16:56:17 -0300

    Update src/App.tsx - 2025-03-21T19:56:02.989Z

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 
-// No explicit React import needed with modern JSX transform
-import LandingPage from './LandingPage';
-import './App.css';
+// For TypeScript with newer React versions, explicit React import isn't required
+// unless using React namespace directly
+import './App.css'
+import LandingPage from './LandingPage'
 
 function App() {
-  // Updated at: 2025-03-21T19:55:39.573Z
-  // Update ID: uapc8ask
+  // This component was last updated at: 2025-03-21T19:56:02.989Z
   return (
     <div className="App">
       <LandingPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
