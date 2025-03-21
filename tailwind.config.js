84e20f7a1e29327e616f48894a5d6c9695f6ae7f
dameiso412 <damaso@sacamedi.com>
2025-03-21 16:55:44 -0300

    Add tailwind.config.js - 2025-03-21T19:55:39.573Z

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4285f4',
+        secondary: '#34a853',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
